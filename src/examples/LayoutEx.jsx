ca59f17f15f0e465c30c634e140c828d6980db5a
James Young <james.s.young@sony.com>
2017-11-29 07:38:49 +0000

    wip

--- src/examples/LayoutEx.jsx
+++ src/examples/LayoutEx.jsx
@@ -47,6 +47,10 @@ class LayoutEx extends MUIBase {
               The Grid component offers a very flexible grid LayoutExt that is responsive.
               (Try resizing this browser and you wil see the Grid adjust
               to the size.)
+
+              You can specify a 'gutter' option property that gives you complete control over the number of pixels
+              that are used between cells. (The default is 24px).  See examples below.
+
             </p>
             <p>
               You can nest Grids (like this page does) to provide even more flexibility.
@@ -54,14 +58,29 @@ class LayoutEx extends MUIBase {
             <p>
               <Typography font='display1'>Span 1 x 12</Typography>
             </p>
-            <Grid>
-
-
+            <Grid >
               <div style={cell} span='1'></div><div style={cell} span='1'></div><div style={cell} span='1'></div>
               <div style={cell} span='1'></div><div style={cell} span='1'></div><div style={cell} span='1'></div>
               <div style={cell} span='1'></div><div style={cell} span='1'></div><div style={cell} span='1'></div>
               <div style={cell} span='1'></div><div style={cell} span='1'></div><div style={cell} span='1'></div>
             </Grid>
+
+            <p>
+              <Typography font='display1'>Span 1 x 12, gutter of 1px</Typography>
+            </p>
+            <Grid gutter='1px'>
+              <div style={cell} span='1'></div><div style={cell} span='1'></div><div style={cell} span='1'></div>
+              <div style={cell} span='1'></div><div style={cell} span='1'></div><div style={cell} span='1'></div>
+              <div style={cell} span='1'></div><div style={cell} span='1'></div><div style={cell} span='1'></div>
+              <div style={cell} span='1'></div><div style={cell} span='1'></div><div style={cell} span='1'></div>
+            </Grid>
+            <p>
+              <Typography font='display1'>Span 2 x 6, gutter of 64px</Typography>
+            </p>
+            <Grid gutter='64px'>
+              <div style={cell} span='6'></div><div style={cell} span='6'></div>
+            </Grid>
+
             <p>
               <Typography font='display1'>Span 4 x 3 Grid</Typography>
             </p>
