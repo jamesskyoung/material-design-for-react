b1071bbd0d51891d3dfc390be68c133a2f14e9b7
James Younge <jamesskyoung@outlook.com>
2017-12-12 18:48:50 +0000

    wip

--- src/examples/ChartsEx.jsx
+++ src/examples/ChartsEx.jsx
@@ -94,6 +94,7 @@ class ChartsEx extends MUIBase {
                   <tr ><td>ringBackground</td><td>HTML Color</td><td>Theme secondary color</td><td>The color of the outer ring</td></tr>
                   <tr ><td>ringColor</td><td>HTML Color</td><td>Theme primary color</td><td>The color of the inner ring -- i.e. the value</td></tr>
                   <tr ><td>ringTextColor</td><td>HTML Color</td><td>black</td><td>The color of the value text</td></tr>
+                  <tr ><td>value</td><td>int</td><td>none</td><td>The value -- between 0-100</td></tr>
 
                 </tbody>
               </table>
