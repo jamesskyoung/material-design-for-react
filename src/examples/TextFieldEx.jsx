9256c2973ac8dfca35f44e006a021e6aa8a604b8
James Young <james.s.young@sony.com>
2017-12-29 20:24:05 +0000

    wip

--- src/examples/TextFieldEx.jsx
+++ src/examples/TextFieldEx.jsx
@@ -57,7 +57,7 @@ class TextFieldEx extends MUIBase {
             <div span='1'></div>
             <div style={{ textAlign: 'left' }} span='11'>
               <Typography font='display1'>About &rarr; <strong>TextField</strong></Typography>
-
+              <Typography font='body1'>
               <table cellSpacing={5} style={{ width: '100%', borderSpacing: '0px', padding: '6px' }}>
                 <thead style={{ padding: '6px' }}>
                   <tr >
@@ -69,16 +69,18 @@ class TextFieldEx extends MUIBase {
                 </thead>
                 <tbody>
                   <tr ><td >id</td><td>id</td><td>none</td><td>The ID of this component</td></tr>
-                  <tr ><td >helpText</td><td>String</td><td>none</td><td>This text will appear under the field -- experimental.</td></tr>
+                  <tr ><td >helpText</td><td>String</td><td>none</td><td>This text will appear under the field -- experimental. (See required)</td></tr>
+                  <tr ><td >minLength</td><td>int</td><td>none</td><td>The minimum length of the field.  Tabbing out of the field when 'incomplete' causes the helpText to turn red.</td></tr>
                   <tr ><td >type</td><td>String</td><td>none</td><td>The type of field (text, password...)</td></tr>
                   <tr ><td >placeholder</td><td>String</td><td>none</td><td>The placeholder text.</td></tr>
                   <tr ><td >onChange</td><td>Function</td><td>none</td><td>Fired on an onChange event</td></tr>
                   <tr ><td >onClick</td><td>Function</td><td>none</td><td>Fired on an onClick event</td></tr>
-                  <tr ><td >required</td><td>boolean</td><td>false</td><td>If true, then '*' is placed beside the placeholder.</td></tr>
+                  <tr ><td >required</td><td>boolean</td><td>false</td><td>If true, then '*' is placed beside the placeholder. Tabbing out of the field when empty causes the helpText to turn red etc. Try the example above.</td></tr>
                 </tbody>
               </table>
               <div style={{ marginTop: '24px' }}></div>
               This page can be viewed here <a href='https://github.com/jamesskyoung/material-design-for-react/blob/master/src/examples/TextFieldEx.jsx' target='_blank'>Here</a>
+              </Typography>
 
               <p>
                 <Typography font='headline'>Code example</Typography>
@@ -95,7 +97,8 @@ class TextFieldEx extends MUIBase {
                     + "\n   this.setState({ currentValue: event.target.value });"
                     + "\n }"
                     + "\n "
-                    + "\n<TextField type='text' id='myid' placeholder='Hover above' "
+                    + "\n<TextField required={true} helpText='Enter...' minLength={8} "
+                    + "\n   type='text' id='myid' placeholder='Hover above' "
                     + "\n   onChange={this.onChange.bind(this)}"
                     + "\n   onClick={this.onClick.bind(this)}>"
                     + "\n/>")}
